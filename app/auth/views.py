ffd8781efa89a9e09bf9b3fe7dc2776a7f894718
Titus Ouko <titusouko@gmail.com>
2019-10-22 16:09:54 +0300

    Add views function for routing

--- app/auth/views.py
+++ app/auth/views.py
@@ -12,8 +12,10 @@ def login():
         user = User.query.filter_by(email=login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
         flash('Invalid username or Password')
-    title = "watchlist login"
+    title = "Pitch login"
     return render_template('auth/login.html',login_form = login_form, title = title)
 
 @auth.route('/logout')
