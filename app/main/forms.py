cbe967e29f5199004a526a95899dc691058dbddd
Titus Ouko <titusouko@gmail.com>
2019-10-22 16:09:21 +0300

    add forms for accessing user data

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField, RadioField,SelectField
 from wtforms.validators import Required
 
-class ReviewForm(FlaskForm):
+class CommentsForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    vote=RadioField('default field arguments', choices=[('1', 'UpVote'), ('1', 'DownVote')])
+    submit = SubmitField('SUBMIT')
 
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review')
-    submit = SubmitField('Submit')
+class PitchForm(FlaskForm):
+    category_id = SelectField('Select Category', choices=[('1', 'Interview'), ('2', 'Pick Up Lines'), ('3', 'Promotion'),('4','Product')])
+    content = TextAreaField('make a pitch', validators=[Required()])
+    submit = SubmitField('Create Pitch') 
+
+class UpvoteForm(FlaskForm):
+    '''
+    Class to create a wtf form for upvoting a pitch
+    '''
+    submit = SubmitField('Upvote')
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
