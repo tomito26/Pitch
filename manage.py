44eddc7006796933fd5edd2c266650797cf2ab92
Titus Ouko <titusouko@gmail.com>
2019-10-22 16:12:41 +0300

    Add instances to be run on runtime

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
-from app.models import User, Role, Review 
+from app.models import User, Comment, Pitch, PitchCategory
 
 
 
@@ -22,7 +22,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User, Role = Role, Review = Review)
+    return dict(app = app, db = db, User = User, Pitch = Pitch, PitchCategory = PitchCategory)
 
     
 if __name__ == '__main__':
