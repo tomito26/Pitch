078778a06e4ca5553d1e4d710b20513e1a393c68
Titus Ouko <titusouko@gmail.com>
2019-10-23 10:10:12 +0300

    update file

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 
-export SECRET_KEY=FlaskSecret
-export MAIL_USERNAME=titusouko@gmail.com
-export MAIL_PASSWORD=haverespect
+export SECRET_KEY='FlaskSecret'
+export MAIL_USERNAME='titusouko@gmail.com'
+export MAIL_PASSWORD='haverespect'
 
 python3.6 manage.py server
